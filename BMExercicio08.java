529ebe89ee8da42a2c31790f3242405c81f27ae2
Pedro <pedrojuniorifpb@gmail.com>
2022-06-08 14:23:19 -0300

    Adicionando a oitava atividade do livro Batista & Moraes

---
+++ BMExercicio08.java
@@ -0,0 +1,9 @@
+class BMExercicio08 {
+    public static void main(String[] args) {
+        for (int cont = 0; cont <= 100; cont++) {
+            if (cont % 3 == 0) {
+                System.out.print(cont + " ");
+            }
+        }
+    }
+}
