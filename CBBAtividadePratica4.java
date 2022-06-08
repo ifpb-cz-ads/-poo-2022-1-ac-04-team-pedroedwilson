02d851bbd060832ec65ed6f5a5e765e45712e14c
Pedro <pedrojuniorifpb@gmail.com>
2022-06-08 13:42:55 -0300

    Adicionado as questões do livro Chagas, Baruque & Baruque

---
+++ CBBAtividadePratica4.java
@@ -0,0 +1,9 @@
+class CBBAtividadePratica4 {
+    public static void main(String[] args) {
+        int cont = 0;
+        while (cont <= 100) {
+            System.out.print(cont + " ");
+            cont++;
+        }
+    }
+}
---
+++ CBBAtividadePratica5.java
@@ -0,0 +1,9 @@
+class CBBAtividadePratica5 {
+    public static void main(String[] args) {
+        int cont = 0;
+        while (cont <= 100) {
+            System.out.print(cont + " ");
+            cont += 2;
+        }
+    }
+}
